565bb3b67a553772e56676230ea6c30231094a77
Admin <acabeza01@hotmail.com>
2021-03-21 20:43:48 +0100

    Scheduler iedere dag om 17:00u

--- src/main/java/com/acabeza/mailer/MailerApplication.java
+++ src/main/java/com/acabeza/mailer/MailerApplication.java
@@ -39,65 +39,23 @@ public class MailerApplication {
  * heroku run java -jar target/mailer-0.0.1-SNAPSHOT.jar
  * heroku run java -Dserver.port=$PORT $JAVA_OPTS -jar target/mailer-0.0.1-SNAPSHOT.jar
  * 
+ * zie Procfile voor Scheduler job met van Heroku CLI:
+ * heroku ps:scale scheduler=1 
+ * 
  */
 	
-	@Component
-	public class ApplicationRunnerBean implements ApplicationRunner {
-		@Override
-		public void run(ApplicationArguments args) throws Exception {
-
-			System.out.println("Started");
-//			sendmail();
-			System.out.println("Ended");
-
-		}
-	}
+//	@Component
+//	public class ApplicationRunnerBean implements ApplicationRunner {
+//		@Override
+//		public void run(ApplicationArguments args) throws Exception {
+//
+//			System.out.println("Mailer started");
+////			sendmail();
+////			System.out.println("Ended");
+//
+//		}
+//	}
 	
-	private void sendmail() throws AddressException, MessagingException, IOException {
-		   Properties props = new Properties();
-		   props.put("mail.smtp.auth", "true");
-		   props.put("mail.smtp.starttls.enable", "true");
-		   props.put("mail.smtp.host", "smtp.office365.com");
-		   props.put("mail.smtp.port", "587");
-		   
-		   String mailSender = System.getenv("mailsender");
-		   String mailPw = System.getenv("mailpw");
-		   
-		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-		      protected PasswordAuthentication getPasswordAuthentication() {
-		         return new PasswordAuthentication(mailSender, mailPw);
-		      }
-		   });
-		   Message msg = new MimeMessage(session);
-		   msg.setFrom(new InternetAddress(mailSender, false));
-		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailSender));
-		   
-		   byte[] array = new byte[7]; // length is bounded by 7
-		   new Random().nextBytes(array);
-		   String generatedSubject = new String(array, Charset.forName("UTF-8"));
 
-		   new Random().nextBytes(array);
-		   String generatedContent = new String(array, Charset.forName("UTF-8"));
-
-		   new Random().nextBytes(array);
-		   String generatedBodyPart = new String(array, Charset.forName("UTF-8"));
-		   
-		   msg.setSubject(generatedSubject);
-		   msg.setContent(generatedContent, "text/html");
-		   msg.setSentDate(new Date());
-
-		   MimeBodyPart messageBodyPart = new MimeBodyPart();
-		   messageBodyPart.setContent(generatedBodyPart, "text/html");
-
-		   Multipart multipart = new MimeMultipart();
-		   multipart.addBodyPart(messageBodyPart);
-
-//		   MimeBodyPart attachPart = new MimeBodyPart();
-//		   attachPart.attachFile("/var/tmp/image19.png");
-//		   multipart.addBodyPart(attachPart);
-
-		   msg.setContent(multipart);
-		   Transport.send(msg);   
-		}	
 	
 }
--- src/main/java/com/acabeza/mailer/ScheduledTasks.java
+++ src/main/java/com/acabeza/mailer/ScheduledTasks.java
@@ -1,7 +1,23 @@
 package com.acabeza.mailer;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +32,57 @@ public class ScheduledTasks {
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 //	@Scheduled(fixedRate = 5000)
-	@Scheduled(cron = "0 0 17 * * *")	//seconden minuten uren dagen maanden jaren
-	public void reportCurrentTime() {
+//	@Scheduled(cron = "3 * * * * *")	//elke minuut op :03 seconden -- seconden minuten uren dagen maanden jaren
+	@Scheduled(cron = "0 0 17 * * *")	//elke dag om 17:00:00 uur -- seconden minuten uren dagen maanden jaren
+	public void reportCurrentTime() throws Exception {
 		log.info("The time is now {}", dateFormat.format(new Date()));
+		sendmail();
 	}
+	
+	private void sendmail() throws AddressException, MessagingException, IOException {
+		   Properties props = new Properties();
+		   props.put("mail.smtp.auth", "true");
+		   props.put("mail.smtp.starttls.enable", "true");
+		   props.put("mail.smtp.host", "smtp.office365.com");
+		   props.put("mail.smtp.port", "587");
+		   
+		   String mailSender = System.getenv("mailsender");
+		   String mailPw = System.getenv("mailpw");
+		   
+		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+		      protected PasswordAuthentication getPasswordAuthentication() {
+		         return new PasswordAuthentication(mailSender, mailPw);
+		      }
+		   });
+		   Message msg = new MimeMessage(session);
+		   msg.setFrom(new InternetAddress(mailSender, false));
+		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailSender));
+		   
+		   byte[] array = new byte[7]; // length is bounded by 7
+		   new Random().nextBytes(array);
+		   String generatedSubject = new String(array, Charset.forName("UTF-8"));
+
+		   new Random().nextBytes(array);
+		   String generatedContent = new String(array, Charset.forName("UTF-8"));
+
+		   new Random().nextBytes(array);
+		   String generatedBodyPart = new String(array, Charset.forName("UTF-8"));
+		   
+		   msg.setSubject(generatedSubject);
+		   msg.setContent(generatedContent, "text/html");
+		   msg.setSentDate(new Date());
+
+		   MimeBodyPart messageBodyPart = new MimeBodyPart();
+		   messageBodyPart.setContent(generatedBodyPart, "text/html");
+
+		   Multipart multipart = new MimeMultipart();
+		   multipart.addBodyPart(messageBodyPart);
+
+//		   MimeBodyPart attachPart = new MimeBodyPart();
+//		   attachPart.attachFile("/var/tmp/image19.png");
+//		   multipart.addBodyPart(attachPart);
+
+		   msg.setContent(multipart);
+		   Transport.send(msg);   
+		}		
 }
