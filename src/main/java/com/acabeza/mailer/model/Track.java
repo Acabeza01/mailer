48e17009127ffe8ee406fb42211f050abd530151
Admin <acabeza01@hotmail.com>
2021-03-24 19:52:21 +0100

    Maven formatter

--- src/main/java/com/acabeza/mailer/model/Track.java
+++ src/main/java/com/acabeza/mailer/model/Track.java
@@ -2,27 +2,26 @@ package com.acabeza.mailer.model;
 
 public class Track {
 
-	private String name;
-	private String prev_url;
-	
-	public Track() {}
+    private String name;
+    private String prev_url;
 
-	public String getName() {
-		return name;
-	}
+    public Track() {
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getPrev_url() {
-		return prev_url;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setPrev_url(String prev_url) {
-		this.prev_url = prev_url;
-	}
-	
-	
-	
+    public String getPrev_url() {
+        return prev_url;
+    }
+
+    public void setPrev_url(String prev_url) {
+        this.prev_url = prev_url;
+    }
+
 }
--- src/main/java/com/acabeza/mailer/service/ClientCredentialsService.java
+++ src/main/java/com/acabeza/mailer/service/ClientCredentialsService.java
@@ -13,59 +13,55 @@ import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 
 public class ClientCredentialsService {
-	
-	  private static final String clientId = System.getenv("Spotify_clientId") ; 
-	  private static final String clientSecret = System.getenv("Spofity_clientSecret"); 
 
-	  private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-	    .setClientId(clientId)
-	    .setClientSecret(clientSecret)
-	    .build();
-	  
-	  private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
-	    .build();
+    private static final String clientId = System.getenv("Spotify_clientId");
+    private static final String clientSecret = System.getenv("Spofity_clientSecret");
 
-	  public static void clientCredentials_Sync() throws Exception {
-	    try {
-	      final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
-	      
-	      if (clientCredentials.getExpiresIn() <= 0) {
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(clientId)
+            .setClientSecret(clientSecret).build();
 
-		      // Set access token for further "spotifyApi" object usage
-		      spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-	      }
-	      
-//	      System.out.println("Expires in: " + clientCredentials.getExpiresIn());
-//	      
-//	      System.out.println("Access token: " + clientCredentials.getAccessToken());
-	      
-	    } catch (IOException | SpotifyWebApiException e) {
-	      System.out.println("Error: " + e.getMessage());
-	    }
-	  }
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials().build();
 
-	  public static String getAccessToken_Sync() {
-		    try {
+    public static void clientCredentials_Sync() throws Exception {
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
 
-		      final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
-		      
-		      // Set access token for further "spotifyApi" object usage
+            if (clientCredentials.getExpiresIn() <= 0) {
 
-//		      System.out.println("Expires in: " + clientCredentials.getExpiresIn());
-//		      
-//		      System.out.println("Access token: " + clientCredentials.getAccessToken());
+                // Set access token for further "spotifyApi" object usage
+                spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+            }
 
-		      return clientCredentials.getAccessToken();
-		      
-		    } catch (IOException | SpotifyWebApiException e) {
-		      System.out.println("Error: " + e.getMessage());
-		      return null;
-		    } catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
-			}
-		
-		  }
+            // System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+            //
+            // System.out.println("Access token: " + clientCredentials.getAccessToken());
+
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static String getAccessToken_Sync() {
+        try {
+
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+            // Set access token for further "spotifyApi" object usage
+
+            // System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+            //
+            // System.out.println("Access token: " + clientCredentials.getAccessToken());
+
+            return clientCredentials.getAccessToken();
+
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
-	  
